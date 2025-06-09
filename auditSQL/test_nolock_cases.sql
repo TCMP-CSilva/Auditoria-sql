b0a0e432bf5adcb87aa2726cfbd38f3d5bbf7fca
Cristian Silva <cristian.silva@tcmpartners.com>
2025-06-09 15:36:15 -0400

    corrección obs

--- auditSQL/test_nolock_cases.sql
+++ auditSQL/test_nolock_cases.sql
@@ -29,13 +29,13 @@ SELECT * FROM dbo.Clientes WITH(NOLOCK);;
 
 -- ❌ Caso 10: JOIN sin NOLOCK en ninguna tabla
 SELECT a.Id, b.Total
-FROM dbo.Ordenes a
-JOIN dbo.DetalleVentas b ON a.Id = b.VentaID;
+FROM dbo.Ordenes a WITH(NOLOCK);
+JOIN dbo.DetalleVentas b WITH(NOLOCK); ON a.Id = b.VentaID;
 
 -- ❌ Caso 11: JOIN con NOLOCK en solo una tabla
 SELECT a.Id, b.Cantidad
 FROM dbo.Ventas a WITH (NOLOCK)
-JOIN dbo.DetalleVentas b ON a.Id = b.VentaID;
+JOIN dbo.DetalleVentas b WITH(NOLOCK); ON a.Id = b.VentaID;
 
 -- ✅ Caso 12: JOIN con NOLOCK en ambas tablas
 SELECT a.Id, b.Cantidad
