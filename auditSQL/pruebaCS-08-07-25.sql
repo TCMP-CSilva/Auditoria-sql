451d26b06ba439f6d29a7e3933ad79fdd13d9221
Cristian Silva <cristian.silva@tcmpartners.com>
2025-07-09 00:47:12 -0400

    corrección

--- auditSQL/pruebaCS-08-07-25.sql
+++ auditSQL/pruebaCS-08-07-25.sql
@@ -2,7 +2,7 @@
 
 -- Caso 1: Falta NOLOCK en tabla física (error)
 SELECT *
-FROM dbo.Clientes;
+FROM dbo.Clientes WITH (NOLOCK);
 
 -- Caso 2: Tiene hint WITH (NOLOCK) en mayúsculas (correcto)
 SELECT *
@@ -18,13 +18,13 @@ FROM dbo.Productos WITH (NOLOCK, READUNCOMMITTED);
 
 -- Caso 5: JOIN sin NOLOCK en segundas tablas (error)
 SELECT a.Id, b.Total
-FROM dbo.Ordenes a
-    JOIN dbo.DetalleVentas b ON a.Id = b.VentaID;
+FROM dbo.Ordenes a WITH (NOLOCK)
+    JOIN dbo.DetalleVentas b WITH (NOLOCK) ON a.Id = b.VentaID;
 
 -- Caso 6: JOIN con hint en primer tabla pero no en segunda (error)
 SELECT a.Id, b.Cantidad
 FROM dbo.Ventas a WITH (NOLOCK)
-    JOIN dbo.DetalleVentas b ON a.Id = b.VentaID;
+    JOIN dbo.DetalleVentas b WITH (NOLOCK) ON a.Id = b.VentaID;
 
 -- Caso 7: JOIN con hint en ambas tablas (correcto)
 SELECT a.Id, b.Cantidad
